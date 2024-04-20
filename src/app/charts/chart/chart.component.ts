e64f3ed13679c975ae6f59136ed6778966c59255
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-04-20 20:20:00 +0200

    Zone Pollution Fix

--- src/app/charts/chart/chart.component.ts
+++ src/app/charts/chart/chart.component.ts
@@ -1,4 +1,14 @@
-import { Component, DestroyRef, effect, ElementRef, inject, input, viewChild } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  effect,
+  ElementRef,
+  inject,
+  input,
+  NgZone,
+  viewChild,
+} from '@angular/core';
 
 import { ChartsDataService } from './charts-data.service';
 import { BlinkService } from '../../shared/blink.service';
@@ -20,19 +30,24 @@ export class ChartComponent {
 
   private chart?: anychart.charts.Pie | null;
 
+  private readonly cdr = inject(ChangeDetectorRef);
   private readonly chartsDataService = inject(ChartsDataService);
   private readonly blinkService = inject(BlinkService);
   private readonly elementRef = inject(ElementRef);
   private readonly destroyRef = inject(DestroyRef);
+  private readonly ngZone = inject(NgZone);
 
   constructor() {
     effect(() => {
       this.chart = anychart.pie(this.chartsDataService.getData(this.data()));
       const container = this.container();
-      if (this.chart && container) {
-        this.chart.container(container.nativeElement);
-        this.chart.draw();
-      }
+      this.ngZone.runOutsideAngular(() => {
+        if (this.chart && container) {
+          this.chart.container(container.nativeElement);
+          this.chart.draw();
+          //this.cdr.detach();
+        }
+      });
     });
 
     this.destroyRef.onDestroy(() => {
