23fb914f0b0f690a841916e8f2d4a193159ef3b5
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-11-20 11:01:02 +0100

    Add RenderModes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,27 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'deferred-charts',
+    renderMode: RenderMode.Client, // render on client
+  },
+
+  {
+    path: 'dynamic-chart',
+    renderMode: RenderMode.Server, // on-demand render on server
+  },
+
+  {
+    path: 'flights/flight-edit/:id',
+    renderMode: RenderMode.Client, // :id needs getPrerenderParams for prerender
+    /*async getPrerenderParams(): Promise<{ id: string }[]> {
+      // API call to get the user IDs
+      const flightIds = await inject(FlightService).getFlightIds();
+      // build an array like [{ id: '1' }, { id: '2' }, { id: '3' }]
+      return flightIds.map((id) => ({ id: '' + id }));
+    },*/
+  },
+
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
