43e3a0c62977b2ed1650147b5eaa355250dcfbbf
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2025-11-17 14:59:31 +0100

    Disbale Angular profiling

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { enableProfiling } from '@angular/core';
+// import { enableProfiling } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-enableProfiling();
+// enableProfiling();
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
